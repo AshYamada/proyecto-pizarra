4c409b7ec22745a52a0380c5d6cb6a30e8cf46f6
ArielLagos <ariellagoslira@email.com>
2024-07-13 23:15:19 -0400

    commit 2

--- src/components/Nota.jsx
+++ src/components/Nota.jsx
@@ -1,3 +1,29 @@
-export function Nota() {
-    
+export function Nota({ nota, setNotas }) {
+  const { id, titulo, descripcion, importancia } = nota;
+
+  if (importancia) {
+    return (
+      <div className="rojo nota">
+        <div>
+          <div className="titulo">{titulo}</div>
+          <div>
+            <button>X</button>
+          </div>
+        </div>
+        <div>{descripcion}</div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="amarillo nota">
+      <div>
+        <div className="titulo">{titulo}</div>
+        <div>
+          <button>X</button>
+        </div>
+      </div>
+      <div>{descripcion}</div>
+    </div>
+  );
 }
--- src/components/Pizarra.jsx
+++ src/components/Pizarra.jsx
@@ -1,6 +1,60 @@
 import { Nota } from "./Nota";
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid'
 
 export function Pizarra() {
+
+    const [listaNotas,setNotas] = useState([
+        {
+            id:uuid(),
+            titulo:'Titulo 1',
+            descripcion:'Descripcion Normal',
+            importancia:false
+        },
+        {
+            id:uuid(),
+            titulo:'Titulo 2',
+            descripcion:'Descripcion Importante',
+            importancia:true
+        },
+        {
+            id:uuid(),
+            titulo:'Relleno 3',
+            descripcion:'Relleno Importante',
+            importancia:true
+        },
+        {
+            id:uuid(),
+            titulo:'Relleno 4',
+            descripcion:'Relleno Normal',
+            importancia:false
+        },
+        {
+            id:uuid(),
+            titulo:'Relleno 5',
+            descripcion:'Relleno Importante',
+            importancia:true
+        },
+        {
+            id:uuid(),
+            titulo:'Relleno 6',
+            descripcion:'Relleno Normal',
+            importancia:false
+        },
+        {
+            id:uuid(),
+            titulo:'Relleno 7',
+            descripcion:'Relleno Importante',
+            importancia:true
+        },
+        {
+            id:uuid(),
+            titulo:'Relleno 8',
+            descripcion:'Relleno Normal',
+            importancia:false
+        }
+    ])
+
   return <div className="container">
       <div>
         <h1>Post It Simulator!</h1>
@@ -14,6 +68,10 @@ export function Pizarra() {
           <button className="btn">Agregar</button>
         </div>
       </div>
-      <div>Notas</div>
+      <div className="contenedor-flex">
+        {
+            listaNotas.map((nota)=>(<Nota nota={nota} setNotas={setNotas}/>))
+        }
+      </div>
     </div>;
 }
